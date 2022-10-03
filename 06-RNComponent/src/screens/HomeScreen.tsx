2443e7df5759b790491161e6368d25f149a092ae
Ezequiel Peralta <ezequiiel.peralta@gmail.com>
2022-10-03 18:46:21 -0300

    RnComponents

--- 06-RNComponent/src/screens/HomeScreen.tsx
+++ 06-RNComponent/src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, View ,FlatList } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { styles } from '../theme/appTheme';
 
 
 interface MenuItem{
@@ -12,25 +13,49 @@ const menuItems=[{
   name:'Animation 101',
   icon:'bookmark-outline',
   components:'Animation101Screen'
-}]
+},
+{
+  name:'battery-dead-outline',
+  icon:'bookmark-outline',
+  components:'Animation101Screen'
+}
+]
 
 export const HomeScreen = () => {
 
   const renderMenu=( menuItem: MenuItem )=>{
     return(
-      <View>
+      <View >
+        
         <Text style={{color:'black'}}>{menuItem.name} - {menuItem.icon}</Text>
       </View>
     )
   }
-
-
+  const renderListHeader=()=>{
+    return(
+      <View style={{marginTop:20, marginBottom:20}}>
+        <Text style={styles.title}>Opciones de Menu</Text>
+      </View>
+    )
+  }
+  const separador=()=>{
+    return(
+      <View style={{borderBottomWidth:2,
+      opacity:0.4,
+      marginVertical:8
+      }}/>
+    )
+  }
   return (
-    <View>
+    <View style={{flex:1,...styles.globalMargin}}>
+      
+
       <FlatList 
       data={ menuItems }
       renderItem={({item})=>renderMenu(item)}
       keyExtractor={(item)=>item.name}
+      ListHeaderComponent={renderListHeader} //genera una lista de "componentes" ubicados en el header
+      ItemSeparatorComponent={separador} //utilizado para separar componentes de la flatList
       /> 
     
   </View>
---
+++ 06-RNComponent/src/theme/appTheme.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    title:{
+        fontSize:35,
+        fontWeight:'bold',
+        color:'black'
+    },
+    globalMargin:{
+        marginHorizontal:20
+    }
+});
