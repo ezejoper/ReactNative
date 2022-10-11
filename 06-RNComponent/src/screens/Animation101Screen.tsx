33178cee9e2e7b32c1f81757a5a9ca7d6e9b59f7
Ezequiel Peralta <ezequiiel.peralta@gmail.com>
2022-10-11 09:09:04 -0300

    UseAnimation

--- 06-RNComponent/src/screens/Animation101Screen.tsx
+++ 06-RNComponent/src/screens/Animation101Screen.tsx
@@ -1,11 +1,72 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native';
+import React, { useRef } from 'react'
+import { Animated, Button, Easing, StyleSheet, View } from 'react-native';
 
 export const Animation101Screen = () => {
+  const opacity= useRef(new Animated.Value(0)).current
+
+  const top=useRef(new Animated.Value(-100)).current
+
+
+  const fadeIn= ()=>{
+    Animated.timing(
+      opacity,
+      {
+        toValue: 1,
+        duration:300,
+        useNativeDriver:true //hace qe la animacion sea por hardwarrwe
+      }
+    ).start( ()=>console.log('animacion terminada')) //siempre se coloca para que comience la animacion
+
+    Animated.timing(
+      top,
+      {
+        toValue:0,
+        duration:900,
+        useNativeDriver:true,
+        easing: Easing.bounce
+      }
+    ).start()
+  }
+  const fadeOut= ()=>{
+    Animated.timing(
+      opacity,
+      {
+        toValue: 0,
+        duration:300,
+        useNativeDriver:true //hace qe la animacion sea por hardwarrwe
+      }
+    ).start() //siempre se coloca para que comience la animacion
+
+    Animated.timing(
+      top,
+      {
+        toValue:-100,
+        duration:900,
+        useNativeDriver:true,
+        easing: Easing.bounce
+      }
+    ).start()
+  }
   return (
     <View style={styles.container}>
-      <View style={styles.purpleBox} />
+      <Animated.View style={{...styles.purpleBox,
+        opacity:opacity,
+        marginBottom:20,
+        transform:[{
+          translateY:top
+        }]
+        }} />
+        <Button
+        title='fadeIn'
+        onPress={fadeIn}
+        />
+        <Button
+        title='fadeOut'
+        onPress={fadeOut}
+        />
+        
     </View>
+
   )
 }
 const styles = StyleSheet.create({
